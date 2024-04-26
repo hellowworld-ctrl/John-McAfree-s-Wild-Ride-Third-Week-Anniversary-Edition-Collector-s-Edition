7e948d370c5fc12d5be430c623ac7180e07ffd0a
JohnG-B <jgbrereton1@my.scccd.edu>
2024-04-26 13:09:08 -0700

    Added the main file.

---
+++ main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include "/public/read.h"
+using namespace std;
+
+
+// John Brereton, responsible for Intergrating everything.
+
+int main(){
+ cout << "Hello World!" << endl;
+
+}
