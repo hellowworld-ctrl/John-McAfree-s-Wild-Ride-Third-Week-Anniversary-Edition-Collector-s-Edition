aa170e1f42318662ba3a28d97d2975faae0e1f9c
0915279 <twsokoloff1@my.scccd.edu>
2024-04-27 18:06:04 -0700

    Weaving the space-time continuum

--- main.cc
+++ main.cc
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <thread> // All multithreading will take place in main - Timothy Sokoloff
 #include "/public/read.h"
 using namespace std;
-
 
 // John Brereton, responsible for Intergrating everything.
 
 int main(){
- cout << "Hello World!" << endl;
-
+	cout << "Hello World!" << endl;
 }
