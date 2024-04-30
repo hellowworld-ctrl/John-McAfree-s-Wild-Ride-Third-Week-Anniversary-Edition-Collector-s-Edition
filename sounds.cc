bd0b0892680f2f41a762ff1869923e440d2f2746
bellucci_0914945 <gianni.bellucci@gmail.com>
2024-04-29 22:23:12 -0700

    Nothing is more precious than independence and liberty. - Ho Chi Minh

--- sounds.cc
+++ sounds.cc
@@ -1,1 +1,33 @@
 // Any sounds, including the soundtrack and various sound design, will be here. - GIANNI 
+#include "Bridges.h"
+#include <AudioClip.h>
+#include <math.h>
+#include "rapidjson/stringbuffer.h"
+#include "base64.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std; 
+using namespace bridges; 
+
+void die() {
+	EXIT_FAILURE; 
+} 
+
+struct WaveHeader {
+		unsigned int sample_rate;
+		unsigned int format_type; 
+		unsigned int data_size; 
+};
+
+int main() {
+	ifstream file ("test.wav" , ios::binary);
+
+	if (!file.is_open()) {
+		cout << "This shit ain't opening" << endl; 
+		die(); 	
+	}
+
+
+}; 
