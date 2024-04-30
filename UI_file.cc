07fd43d9a536d59723e790bb2e863e0d6b639a47
burford_0781254 <0781254@my.scccd.edu>
2024-04-29 22:26:27 -0700

    All that I see, absolute horror!

--- UI_file.cc
+++ UI_file.cc
@@ -2,9 +2,13 @@
 //Camila was here 🐩🐩🐩
 //THERE SHALL BE CODE HERE SOMEDAY!😢😢😢
 #include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#include <SDL.h>
 #include <iostream>
 using namespace std;
 
 int main() {
 	
-}
+}`
