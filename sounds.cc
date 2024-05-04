502ea4f6e0b06403f0e02a59995970b9990773c6
Gianni J. Bellucci <96031099+scrumpD@users.noreply.github.com>
2024-05-04 14:31:28 -0700

    these sounds should work (only beach and main)

--- sounds.cc
+++ sounds.cc
@@ -1,58 +1,88 @@
 // Any sounds, including the soundtrack and various sound design, will be here. - GIANNI 
-#include <iostream>
-#include <string>
-#include "ColorGrid.h"
-#include "Bridges.h"
-#include "Color.h"
 #include "AudioClip.h"
+#include <iostream>
+#include <fstream>
+#include "Bridges.h"
+#include <math.h>
 
 using namespace std;
 using namespace bridges;
 
-int main(int argc **argv){
-    // Initialize Bridges
-    Bridges bridges (ASSIGNMENT_NUMBER, USER_ID, API_KEY);
-
-    // set title and description
-    bridges.setTitle("Audio Wave");
-    bridges.setDescription("Create the wave form of an audio file");
+AudioClip mixFadeClips(const AudioClip& ac1, const AudioClip& ac2, int fadeDuration, int duration) {
+  if (ac1.getSampleRate() != ac2.getSampleRate()) {
+    throw "can't mix audio clips of varying rates";
+  }
+  
+  // sample count = sample rate * number of seconds
+  int SAMPLE_RATE = ac1.getSampleRate();
+    int sampleCount = SAMPLE_RATE * duration;
     
-        /*
-        use different colors for the background of the color grid and the wave
-        */ 
-        ColorGrid cg = ColorGrid(1080, 1920, color);
-        
-        /*
-        get the audio file by using its name
-        */
-        
-        // get samples from the audio file
-        
+    AudioClip acMix = AudioClip(sampleCount, 1, ac1.getSampleBits(), SAMPLE_RATE);
+    
+    // Get half of the song for the fade center point
+    int center = acMix.getSampleCount() / 2;
+
+    //  Get the number of samples that the songs should fade in/out
+    int fadeSamples = SAMPLE_RATE * fadeDuration;
+    
+    // Ensure the fade duration does not exceed the clip duration
+    fadeSamples = min(fadeSamples, acMix.getSampleCount());
+    
+    // Get half of the fade samples for easier centering on the clip
+    // ensure the samples are correctly split when of an odd number
+    int fadeHalf1 = fadeSamples / 2;
+    int fadeHalf2 = fadeHalf1 - (fadeSamples / 2);
+    
+    // Calculate the start, end, and length of the fade section
+    int start = center - fadeHalf1;
+    int end = center + fadeHalf2;
+    
+    // Loop through the samples of both clips and use the average amplitude value
+    for (int i = 0; i < acMix.getSampleCount(); i++) {
+        // Get the two samples to be mixed
+        // sample1 starts at full amplitude and fades out after start until end
+        // sample2 starts at 0 amplitude and fades in after start until end
+        int sample1 = (int)(ac1.getSample(0, i) * (max(0, fadeSamples - (i - start)) / (float)fadeSamples));
+        int sample2 = 0;
+
+        if (i >= start)
+            sample2 = (int)(ac2.getSample(0, i) * (min(fadeSamples, (i - start)) / (float)fadeSamples));
             
-        /*
-        average the samples.
+        acMix.setSample(0, i, (sample1 + sample2) / 2);
+    }
+    
+    return acMix;
+}
+
+AudioClip mixFadeClips(AudioClip ac1, AudioClip ac2) {
+    // If no duration is given then just use the minimum song duration
+    int duration = min(ac1.getSampleCount() / ac1.getSampleRate(), ac2.getSampleCount()  / ac2.getSampleRate());
+
+    // If no fade duration is given then use half the total duration
+    int fadeDuration = duration / 2;
+    
+    return mixFadeClips(ac1, ac2, fadeDuration, duration);
+}
+
+int main() {
+    Bridges bridges = Bridges(133, "BRIDGES_USER_ID", "BRIDGES_API_KEY");
+    bridges.setTitle("Audio Mixing");
+    bridges.setDescription("Fading two audio clips together");
+
+    // Load the clips
+
+    AudioClip mainMenu = AudioClip("menu.wav");
+    bridges.setDataStructure(mainMenu);
+    bridges.visualize();
+
+    AudioClip theBeach = AudioClip("beach theme.wav") 
+    bridges.setDataStructure(theBeach);
+    bridges.visualize();
         
-        keep in mind that the audio's sampling rate
-        is given in samples per second
-        
-        samples range from -1 to 1
-        */
-        
-        /*
-        plot the averages onto the color grid to 
-        create the wave form of the audio
-        
-        keep in mind that the origin of the color grid
-        is on the top left corner with y increasing 
-        downwards
-        */
-        
-        /*
-        fill the wave form starting from the middle of the grid
-        to the point that was plotted earlier
-        */
-       
-       // visualization
-        bridges.setDataStructure(cg);
-        bridges.visualize();
+    // Apply fade mixing to the two AudioClips and visualize
+    AudioClip acFadeMix = mixFadeClips(mainMenu, theBeach);
+    bridges.setDataStructure(acFadeMix);
+    bridges.visualize();
+
+    return 0;
 }
