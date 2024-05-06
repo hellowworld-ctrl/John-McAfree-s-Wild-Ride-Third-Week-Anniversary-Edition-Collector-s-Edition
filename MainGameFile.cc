ff528d5668d3458421c23820682508779830c3be
JohnG-B <jgbrereton1@my.scccd.edu>
2024-05-06 09:44:41 -0700

    Main Game stuff

--- MainGameFile.cc
+++ MainGameFile.cc
@@ -6,6 +6,9 @@
 #include "Map stuff/MapRoad.h"
 #include "Map stuff/MapCity.h"
 #include "Map stuff/MapAirport.h"
+#include "inherit/combat.h"
+#include "inherit/generate.h"
+#include "inherit/tier.h"
 #include <stdlib.h>
 #include <ncurses.h>
 #include <random>
@@ -13,13 +16,11 @@
 #include <signal.h>
 #include <iostream>
 #include <cctype>
-#include <unistd.h>
-#include "inherit/combat.h"
-#include "inherit/generate.h"
-#include "inherit/tier.h"
+#include <thread>
 
 using namespace std;
 
+shared_ptr<Party<Hero>> mainParty;
 
 void Menu() {
 	static MapHouse map;
@@ -46,22 +47,22 @@ void Menu() {
 		choice = wgetch(menuwin);
 
 		switch (choice) {
-		case 'w':
-			highlight--;
-			if (highlight == -1)
+			case 'w':
+				highlight--;
+				if (highlight == -1)
 				highlight = 0;
 			break;
-		case 's':
-			highlight++;
-			if (highlight == 3)
+			case 's':
+				highlight++;
+				if (highlight == 3)
 				highlight = 2;
 			break;
-		default:
+			default:
 			break;
 		}
 		wrefresh(menuwin);
 		if (choice == '\r')
-			break;
+		break;
 
 	}
 	if (highlight == 0) {
@@ -74,7 +75,17 @@ void Menu() {
 	} else if (highlight == 1) {
 		//TODO Display inventory using a menu thingie 👌
 		highlight = 0;
-		clear();
+	    int size = mainParty->get_inventory()->get_items().size();
+
+		for(int i = 0; i < 0; i++)
+		{
+          string title = mainParty->get_inventory()->get_items().at(i)->get_title();
+			wmove(menuwin, i + 5, 1);
+			waddstr(menuwin, title.c_str());
+		}
+			wrefresh(menuwin);
+
+		//clear();
 		for (int i = 0; i < inventory.size(); i++) {
 			if (i == highlight) wattron(menuwin, A_REVERSE);
 			wmove(menuwin, i + 1, 1);
@@ -93,17 +104,17 @@ void Menu() {
 			wrefresh(menuwin);
 			choice = getch();
 			switch (choice) {
-			case 'w':
-				highlight--;
-				if (highlight < 0)
+				case 'w':
+					highlight--;
+					if (highlight < 0)
 					highlight = 0;
 				break;
-			case 's':
-				highlight++;
-				if (highlight >= inventory.size())
-					highlight = inventory.size() - 1;
+				case 's':
+					highlight++;
+					if (highlight >= inventory.size())
+						highlight = inventory.size() - 1;
 				break;
-			default:
+				default:
 				break;
 			}
 		}
@@ -128,83 +139,81 @@ void Menu() {
 	wrefresh(menuwin);
 }
 
+    void CombatMode()
+    {
+        shared_ptr<Party<Monster>> MonsterCombat = Generate::generate_party<Monster>(Tier::Common, 5);
+
+        int x = 30, y = 30, yMax = 50, xMax = 50;
+        // moves the cursor
+        move(y, x);
+        getmaxyx(stdscr, yMax, xMax);
+
+        WINDOW * puzzwin = newwin(18, xMax / 2, yMax / 2, 80);
+
+        box(puzzwin, 0, 0);
+        refresh();
+        wborder(puzzwin, '|', '|', '-', '-', '/', '*', '+', '+');
+        mvwprintw(puzzwin, 1, 35, "A Fight has Broken Out!");
+        wrefresh(puzzwin);
+
+        // makes it so we can use arrow keys
+        keypad(puzzwin, true);
+
+        string choices[4] = {"Fight.", "", "", ""};
+        int choice;     
+        int highlight = 1;     
+
+        shared_ptr<Combat> combat = make_shared<Combat>(mainParty, MonsterCombat);
+        int amountAdded = 0;
+
+        string finalBout;
+
+        while (true) {
+        // Prepare to capture the result of the next combat.
+        pair<string, bool> result;
+        // Let the next combatant attack a random target.
+        result = combat->do_combat();
+         if(!result.second){
+            finalBout = result.first;
+            break;
+        }
+        mvwprintw(puzzwin, amountAdded+3, 35, result.first.c_str());  
+        wrefresh(puzzwin);
+        // The first result will describe the combat as a string.
+         //cout << result.first << endl;
+
+         amountAdded += 1;
+         sleep(1);
+        }
+
+        wrefresh(puzzwin);
+        move(20, 105);
+
+        start_color();
+        init_pair(1, COLOR_GREEN, COLOR_BLACK);
+        init_pair(2, COLOR_RED, COLOR_BLACK);
+
+        usleep(5);
+        usleep(500'000);
+        if (finalBout == "Heroes won the fight.") {
+            attron(COLOR_PAIR(1));
+            move(22, 105);
+            printw(finalBout.c_str());
+            attroff(COLOR_PAIR(1));
+        } else {
+            attron(COLOR_PAIR(2));
+            move(22, 105);
+            printw(finalBout.c_str());
+            attroff(COLOR_PAIR(2));
+        }
+
+        getch();
+        clear();
+        refresh();
+    }
 
 
-shared_ptr<Party<Hero>> mainParty;
 
-void CombatMode()
-{
-	shared_ptr<Party<Monster>> MonsterCombat = Generate::generate_party<Monster>(Tier::Common, 5);
-	
-	int x = 30, y = 30, yMax = 50, xMax = 50;
-	// moves the cursor
-	move(y, x);
-	getmaxyx(stdscr, yMax, xMax);
-
-	WINDOW * puzzwin = newwin(18, xMax / 2, yMax / 2, 80);
-
-	box(puzzwin, 0, 0);
-	refresh();
-	wborder(puzzwin, '|', '|', '-', '-', '/', '*', '+', '+');
-	mvwprintw(puzzwin, 1, 35, "A Fight has Broken Out!");
-	wrefresh(puzzwin);
-
-	// makes it so we can use arrow keys
-	keypad(puzzwin, true);
-
-	string choices[4] = {"Fight.", "", "", ""};
-	int choice;
-	int highlight = 1;
-
-	shared_ptr<Combat> combat = make_shared<Combat>(mainParty, MonsterCombat);
-	int amountAdded = 0;
-    
-	string finalBout;
-
-	while (true) {
-	// Prepare to capture the result of the next combat.
-	pair<string, bool> result;
-	// Let the next combatant attack a random target.
-	result = combat->do_combat();
-	 if(!result.second){ 
-		finalBout = result.first;
-		break;
-	}
-	mvwprintw(puzzwin, amountAdded+3, 35, result.first.c_str());
-	wrefresh(puzzwin);
-	// The first result will describe the combat as a string.
-	 //cout << result.first << endl;
-	 
-	 amountAdded += 1;
-	 sleep(1);
-	}
-
-	wrefresh(puzzwin);
-	move(20, 105);
-
-	start_color();
-	init_pair(1, COLOR_GREEN, COLOR_BLACK);
-	init_pair(2, COLOR_RED, COLOR_BLACK);
-
-	usleep(5);
-    usleep(500'000);
-	if (finalBout == "Heroes won the fight.") {
-		attron(COLOR_PAIR(1));
-		move(22, 105);
-		printw(finalBout.c_str());
-		attroff(COLOR_PAIR(1));
-	} else {
-		attron(COLOR_PAIR(2));
-		move(22, 105);
-		printw(finalBout.c_str());
-		attroff(COLOR_PAIR(2));
-	}
-	
-	getch();
-	clear();
-	refresh();
-
-}
 
 template<typename MapType>
 void Move(const int &ch, MapType&map, WINDOW *win) {
@@ -213,8 +222,6 @@ void Move(const int &ch, MapType&map, WINDOW *win) {
 
 	int newX = x;
 	int newY = y;
-	start_color();
-    init_pair(10,COLOR_RED,COLOR_RED);
 
 	if (ch == 'W' || ch == KEY_UP) newY -= 1;
 	else if (ch == 'A' || ch == KEY_LEFT) newX -= 1;
@@ -222,25 +229,28 @@ void Move(const int &ch, MapType&map, WINDOW *win) {
 	else if (ch == 'D' || ch == KEY_RIGHT) newX += 1;
 
 	if (map.isEnemy(newX, newY) == true) {
-		refresh();
-		getch();
-		clear();
-		refresh();
 		CombatMode();
-		
-        map.map.at(newY).at(newX) = map.OPEN;
-
+		map.map.at(newY).at(newX) = map.OPEN;
 		wmove(win, y, x);
-        wattron(win, COLOR_PAIR(10));
-		mvwprintw(win,newY, newX," ");
-        wattroff(win, COLOR_PAIR(10));
 		wrefresh(win);
 	} else if (map.isChest(newX, newY) == true) {
+		mvprintw(0, 80, "OPENED A CHEST!");
+	    shared_ptr<Weapon> weapon;
+		int randomChance = rand() % 10 + 1;
+
+		if(randomChance >= 9)weapon = Generate::generate_weapon(Tier::Legendary);
+		else if(randomChance >= 6)weapon = Generate::generate_weapon(Tier::Rare);
+		else weapon = Generate::generate_weapon(Tier::Uncommon);
+	
+		string itemName = "You obtained a " + weapon->get_title();
+
+		mvprintw(5, 80, itemName.c_str());
+		
+		mainParty->get_inventory()->add_item(weapon);
 		refresh();
 		getch();
 		clear();
 		refresh();
-	    map.puzz();
 	} else if (map.getPlayerLoc(newX, newY)) {
 		map.screen(newX, newY);
 		wmove(win, newY, newX);
@@ -249,11 +259,12 @@ void Move(const int &ch, MapType&map, WINDOW *win) {
 		wmove(win, y, x);
 		wrefresh(win);
 	}
+
+//	flushinp();
 }
 
-
-
 int main() {
+	srand(time(NULL));
 	int playerMove = 0;
 	int FPS = 60;
 	initscr();
@@ -262,12 +273,13 @@ int main() {
 	nonl();
 	cbreak();
 	noecho();
+	bool keyBeach = true;
+	bool keyRoad = true;
+	bool keyCity = true;
+	bool keyAirport = true;
 
-	
-    mainParty  = Generate::generate_party<Hero>(Tier::Legendary, 5);
-
-	return 0;
-
+    mainParty = Generate::generate_party<Hero>(Tier::Rare, 5);
+label:
 	/*
 	system("figlet FREEDOM FIGHTERS - John McAfree");
 	string startButton = read("Enter \'p\' to go into the world!\n");
@@ -283,7 +295,7 @@ int main() {
 	// moves the cursor
 
 	getmaxyx(stdscr, yMax, xMax);
-	WINDOW * mainwin = newwin(yMax / 2, xMax / 2, 1, 1);
+	WINDOW * mainwin = newwin(yMax/2, xMax/2, 1, 1);
 
 	start_color();
 	if (has_colors()) {
@@ -313,43 +325,44 @@ int main() {
 	int highlight = 0;
 	string Choices[5] = {"Level 1 - House", "Level 2 - Beach", "Level 3 - Road", "Level 4 - City", "Level 5 - Airport"};
 
-	while (true) {
+	while(true) {
 		for (int i = 0; i < 5; i++) {
 			wattron(mainwin, COLOR_PAIR(5));
-			mvwprintw(mainwin, i + 4, 4, Choices[i].c_str());
+			mvwprintw(mainwin, i+4, 4, Choices[i].c_str());
 			wattroff(mainwin, COLOR_PAIR(5));
 		}
 		for (int i = 0; i < 5; i++) {
 			if (i == highlight) {
 				wattron(mainwin, A_REVERSE | COLOR_PAIR(6));
-				mvwprintw(mainwin, i + 4, 4, Choices[i].c_str());
+				mvwprintw(mainwin, i+4, 4, Choices[i].c_str());
 				wattroff(mainwin, A_REVERSE | COLOR_PAIR(6));
 			}
 		}
 		choice = wgetch(mainwin);
 
-		switch (choice) {
-		case 'w' :
-			highlight--;
-			if (highlight == -1)
-				highlight = 0;
+		switch(choice) {
+			case 'w' :
+				highlight--;
+				if(highlight == -1)
+					highlight = 0;
 			break;
-		case 's' :
-			highlight++;
-			if (highlight == 5)
-				highlight = 4;
+			case 's' :
+				highlight++;
+				if(highlight == 5)
+					highlight = 4;
 			break;
-		default:
+			default:
 			break;
 		}
-		if (choice == '\r')
-			break;
+		if(choice == '\r')
+		break;
 	}
 	wrefresh(mainwin);
 	clear();
 	refresh();
 	WINDOW * playwin = newwin(100, 100, 50, 50);
 	wmove(playwin, 50, 50);
+
 
 	if (highlight == 0) {
 		MapHouse map;
@@ -364,18 +377,26 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				map.puzz();
+				if (map.puzz()) {
+					keyBeach = true;
+					clear();
+					refresh();
+					getch();
+					goto label;
+				}
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
 
-			else {
+				else {
 				playerMove = toupper(playerMove);
+				map.monsterMove();
 				Move(playerMove, map, playwin);
 
 			}
 		}
-	} else if (highlight == 1) {
+	}
+	else if (highlight == 1 and keyBeach == true) {
 		MapBeach map;
 		map.lore();
 		clear();
@@ -388,18 +409,26 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				map.puzz();
+				if(map.puzz()) {
+					keyRoad = true;
+					clear();
+					refresh();
+					getch();
+					goto label;
+				}
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
 
-			else {
+				else {
 				playerMove = toupper(playerMove);
+				map.monsterMove();
 				Move(playerMove, map, playwin);
 
 			}
 		}
-	} else if (highlight == 2) {
+	}
+	else if (highlight == 2 and keyRoad == true) {
 		MapRoad map;
 		map.lore();
 		clear();
@@ -412,18 +441,26 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				map.puzz();
+				if(map.puzz()) {
+					keyCity = true;
+					clear();
+					refresh();
+					getch();
+					goto label;
+				}
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
 
-			else {
+				else {
 				playerMove = toupper(playerMove);
+				map.monsterMove();
 				Move(playerMove, map, playwin);
 
 			}
 		}
-	} else if (highlight == 3) {
+	}
+	else if (highlight == 3 and keyCity == true) {
 		MapCity map;
 		map.lore();
 		clear();
@@ -436,18 +473,26 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				map.puzz();
+				if(map.puzz()) {
+					keyAirport = true;
+					clear();
+					refresh();
+					getch();
+					goto label;
+				}
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
 
-			else {
+				else {
 				playerMove = toupper(playerMove);
+				map.monsterMove();
 				Move(playerMove, map, playwin);
 
 			}
 		}
-	} else if (highlight == 4) {
+	}
+	else if (highlight == 4 and keyAirport == true) {
 		MapAirport map;
 		map.lore();
 		clear();
@@ -460,42 +505,36 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				map.puzz();
+				if (map.puzz()) {
+					clear();
+					refresh();
+					WINDOW * cong = newwin(100, 80, 0, 0);
+					box(cong, 0, 0);
+			wattron(cong, COLOR_PAIR(2));
+					mvwprintw(cong, 25, 25, "YOUR FREE TRIAL HAS ENDED:");
+					mvwprintw(cong, 28, 25, "https://www.mcafee.com/en-us/antivirus.html");
+			wattroff(cong, COLOR_PAIR(2));
+					wrefresh(cong);
+					getch();
+					break;
+				}
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
 
-			else {
+				else {
 				playerMove = toupper(playerMove);
+				map.monsterMove();
 				Move(playerMove, map, playwin);
 
 			}
 		}
+	} else {
+		mvwprintw(mainwin, 25, 90, "LEVEL NOT UNLOCKED!");
+		wrefresh(mainwin);
+		getch();
+		goto label;
 	}
-
-	/*
-	map.screen(50, 50);
-	while ((playerMove = wgetch(stdscr)) != 'q') {
-
-		cin >> playerMove;
-
-		if (playerMove == KEY_F(1)) {
-			Menu();
-		} else if (playerMove == KEY_F(2)) {
-			puzz();
-		}
-
-		else if (playerMove == ERR) usleep(1'000'000 / FPS);
-
-			else {
-			playerMove = toupper(playerMove);
-			Move(playerMove, map, playwin);
-
-		}
-	}
-	*/
-
-//else usleep(500'00);
 
 	attroff(COLOR_PAIR(1));
 	clear();
