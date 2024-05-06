d1604ebd690bb5fbaae6fc5cc24e76b1bff53494
burford-0781254 <156368768+burford-0781254@users.noreply.github.com>
2024-05-05 23:47:58 -0700

    Fixed the puzzle mechanic on MainGameFile

--- MainGameFile.cc
+++ MainGameFile.cc
@@ -129,82 +129,6 @@ void Menu() {
 }
 
 
-void puzz() {
-	//int height = 0, width = 0;
-	int x = 30, y = 30, yMax = 50, xMax = 50;
-	// moves the cursor
-	move(y, x);
-	getmaxyx(stdscr, yMax, xMax);
-
-	WINDOW * puzzwin = newwin(18, xMax / 2, yMax / 2, 80);
-
-	box(puzzwin, 0, 0);
-	refresh();
-	wborder(puzzwin, '|', '|', '-', '-', '/', '*', '+', '+');
-	mvwprintw(puzzwin, 1, 1, "Which company did John McAfee found after leaving McAfee Associates(1994)?");
-	mvwprintw(puzzwin, 2, 2, "Press [enter] to select an answer. :)");
-	wrefresh(puzzwin);
-
-	// makes it so we can use arrow keys
-	keypad(puzzwin, true);
-
-	string choices[4] = {"Bloatware.", "Tribal Voice?", "*sniffs* Got any coke?", "Get out of my way commie!"};
-	int choice;
-	int highlight = 0;
-
-	while (true) {
-		for (int i = 0; i < 4; i++) {
-			mvwprintw(puzzwin, i + 3, 4, choices[i].c_str());
-		}
-		for (int i = 0; i < 4; i++) {
-			if (i == highlight) {
-				wattron(puzzwin, A_REVERSE);
-				mvwprintw(puzzwin, i + 3, 4, choices[i].c_str());
-				wattroff(puzzwin, A_REVERSE);
-			}
-		}
-		choice = wgetch(puzzwin);
-
-		switch (choice) {
-		case 'w':
-			highlight--;
-			if (highlight == -1)
-				highlight = 0;
-			break;
-		case 's':
-			highlight++;
-			if (highlight == 4)
-				highlight = 3;
-			break;
-		default:
-			break;
-		}
-		if (choice == '\r')
-			break;
-	}
-	wrefresh(puzzwin);
-	move(20, 75);
-	printw("YOU CHOSE: %s", choices[highlight].c_str());
-	start_color();
-	init_pair(1, COLOR_GREEN, COLOR_BLACK);
-	init_pair(2, COLOR_RED, COLOR_BLACK);
-
-	if (highlight == 1) {
-		attron(COLOR_PAIR(1));
-		move(22, 75);
-		printw("CORRECT! YOU WIN A PRIZE!");
-		attroff(COLOR_PAIR(1));
-	} else {
-		attron(COLOR_PAIR(2));
-		move(22, 75);
-		printw("INCORRECT! REFRESH ON YOUR (John McAfee) KNOWLEDGE!");
-		attroff(COLOR_PAIR(2));
-	}
-
-	getch();
-	clear();
-	refresh();
-}
 
 shared_ptr<Party<Hero>> mainParty;
 
@@ -316,7 +240,7 @@ void Move(const int &ch, MapType&map, WINDOW *win) {
 		getch();
 		clear();
 		refresh();
-	    puzz();
+	    map.puzz();
 	} else if (map.getPlayerLoc(newX, newY)) {
 		map.screen(newX, newY);
 		wmove(win, newY, newX);
@@ -437,7 +361,7 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				puzz();
+				map.puzz();
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
@@ -458,7 +382,7 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				puzz();
+				map.puzz();
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
@@ -479,7 +403,7 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				puzz();
+				map.puzz();
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
@@ -500,7 +424,7 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				puzz();
+				map.puzz();
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
@@ -521,7 +445,7 @@ int main() {
 			if (playerMove == KEY_F(1)) {
 				Menu();
 			} else if (playerMove == KEY_F(2)) {
-				puzz();
+				map.puzz();
 			}
 
 			else if (playerMove == ERR) usleep(1'000'000 / FPS);
